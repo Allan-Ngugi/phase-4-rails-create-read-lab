02e6bb26e39cd2eea8a761547c815d40421066fd
Allan-Ngugi <blessedallan1@gmail.com>
2022-11-24 13:52:33 +0300

    Solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do 
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html  
+  resources :plants, only: [:index, :show, :create]
+
+  # get 'plats/new', to: 'plans#new'
+  # post '/plants', to: 'plants#create'
+  # get '/plants/:id/edit', to: 'plants#edit'
+  # patch '/plants/:id', to: 'plants#update'
+  # delete '/plants/:id', to: 'plants#destroy'
 end
